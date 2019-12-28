25d28d8425865d58c7b918807df8f9ed3f0fcec7
Wataru-Miyamoto <ponmiyapon43@gmail.com>
2019-12-28 13:39:55 +0000

    static_pagesコントローラを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/top'
+
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
+  
+  get     '/login', to: 'sessions#new'
+  post    '/login', to: 'sessions#create'
+  delete  '/logout', to: 'sessions#destroy'
+  
+  
+  resources :users do
+    member do
+      get   'edit_basic_info'
+      patch 'update_basic_info'
+      get   'attendances/edit_one_month'
+      patch 'attendances/update_one_month'
+      
+    end
+    resources :attendances, only: :update
+  end
 end

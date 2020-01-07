7cf9cac122bcd3deb6a12e79b36ce24742ce11b3
Wataru-Miyamoto <ponmiyapon43@gmail.com>
2020-01-07 13:35:47 +0000

    controllerを準備

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,88 @@
 class UsersController < ApplicationController
-  def new
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:index, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :admin_or_correct_user, only: [:show, :edit, :update]
+  before_action :set_one_month, only: :show
+  
+  def index
+    @user = User.search(params[:search])
+    @title = "ユーザー一覧"
+      if params.has_key?(:search)
+        @title = "検索結果"
+      end
+    @users = User.paginate(page: params[:page])
   end
+  
+  def show
+    @worked_sum = @attendances.where.not(started_at: nil).count
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      render :edit  
+    end
+  end
+  
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
+  
+  def edit_basic_info
+  end
+  
+  def update_basic_info
+    @users = User.all
+      @users.each do |users|
+        if users.update_attributes(basic_info_params)
+          flash[:success] = "基本情報を更新しました。"
+        else
+          flash[:danger] = "更新が失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+        end
+      end
+    redirect_to users_url
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation, :search)
+    end
+    
+    def basic_info_params
+      params.require(:user).permit(:basic_time, :work_time)
+    end
+    
+    def admin_or_correct_user
+    @user = User.find(params[:id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "アクセス権限がありません。"
+        redirect_to(root_url)
+      end
+    end
+
 end
